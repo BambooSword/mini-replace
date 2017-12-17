3bcaa91d108af0a6ebd5c13391df38cb76b623bf
BambooSword <549290567@qq.com>
2017-12-17 22:13:04 +0800

    commit the basic code

---
+++ index.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const readline = require('readline');
+
+// step1 找到要修改的文件集合
+let files = []
+function ScanDir(path) {
+  let that = this
+  if (fs.statSync(path).isFile()) {
+    return files.push(path)
+  }
+  try {
+    fs.readdirSync(path).forEach(function (file) {
+      ScanDir.call(that, path + '/' + file)
+    })
+  } catch (e) {
+      console.error(e);
+  }
+}
+
+ScanDir('./demo');
+console.log(process.cwd());
+console.log(files)
+// step2 遍历集合中的文件，匹配对应的正则，找到合适修改内容，把内容替换
+files.forEach((item)=>{
+    let data=fs.readFileSync(item,"utf-8");  
+    //console.log(data); 
+    let reg = /https:\/\/unpkg.com\/bizcharts@3.0.3\/umd\/BizCharts.min.js/g;
+    console.log(data);
+    let newdata = data.replace(reg,'https:\/\/unpkg.com\/bizcharts@3.0.4\/umd\/BizCharts.min.js');
+    console.log(newdata);
+    fs.writeFileSync(item,newdata);
+});
