b7da831f2804d7f2733884790574caa55ec75e4d
ISS\bamboo <wb-dlm292508@alibaba-inc.com>
2018-01-02 15:41:51 +0800

    修改两处

--- test/test.js
+++ test/test.js
@@ -4,5 +4,6 @@ console.log(miniReplace);
 miniReplace(process.cwd(), /hello/, 'ddd');
 
 // this is a test.
+// this is the best.
 // reg = /('|")(\S+)(\1)/g 匹配括号里面的字符
 // reg = /('|")((\S*(?=bizcharts-plugin-slider@2.0.0))(\S+))(\1)/ //实现模糊查询的正则
