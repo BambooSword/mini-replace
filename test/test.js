1c1f4752db4d7bd1db9c021162c9e5cece3cc1cb
ISS\bamboo <wb-dlm292508@alibaba-inc.com>
2018-01-02 15:47:57 +0800

    ti

--- test/test.js
+++ test/test.js
@@ -4,10 +4,6 @@ console.log(miniReplace);
 miniReplace(process.cwd(), /hello/, 'ddd');
 
 // this is a test.
-<<<<<<< HEAD
 // this is the best.
-=======
-// this is an accident.
->>>>>>> ssss
 // reg = /('|")(\S+)(\1)/g 匹配括号里面的字符
 // reg = /('|")((\S*(?=bizcharts-plugin-slider@2.0.0))(\S+))(\1)/ //实现模糊查询的正则
