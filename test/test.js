d8fc3cdd5c15870544faee26853010cbc5ce1b3e
ISS\bamboo <wb-dlm292508@alibaba-inc.com>
2018-01-02 14:54:50 +0800

    测试使用rebase

--- test/test.js
+++ test/test.js
@@ -3,5 +3,6 @@ console.log('hello world');
 console.log(miniReplace);
 miniReplace(process.cwd(), /hello/, 'ddd');
 
+// this is a test.
 // reg = /('|")(\S+)(\1)/g 匹配括号里面的字符
 // reg = /('|")((\S*(?=bizcharts-plugin-slider@2.0.0))(\S+))(\1)/ //实现模糊查询的正则
